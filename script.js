911baa9cf0f792f3c180fc0b321f302414afbbf7
CaioLopes5556 <caiohenrique7772@gmail.com>
2024-08-22 17:55:29 -0300

    pegando os valores dos botoes e inserindo no timer

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+function insert(num){
+    
+    if( document.querySelector('.display').value.length < 4){
+
+
+        let timer = document.querySelector('.display').value
+        document.querySelector('.display').value = timer + num
+
+
+    }
+
+
+}
 
 
