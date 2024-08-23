6810a3a12af68f5d72ede1ec0866a18e1eb521e0
CaioLopes5556 <caiohenrique7772@gmail.com>
2024-08-23 16:23:22 -0300

    adicionando funções, coméntarios, mais  estilizações, fontes e efeitos

--- script.js
+++ script.js
@@ -1,15 +1,45 @@
+// pega a div do botão iniciar pelo ID
+var Btn = document.querySelector('#bt');
+//pega a div do gif
+var food = document.querySelector('.micro-food');
+
+// faz o gif sumir ou aparecer quando o botão inicar é clicado 
+// (apenas se o timer tiver preenchido)
+Btn.addEventListener('click', function(){
+    if(food.style.display === 'none' && document.querySelector('.display').value != 0){
+        food.style.display = 'flex';
+
+    }else{
+        food.style.display = 'none'
+    }
+});
+
+
+// insere numeros no timer do microondas
+
 function insert(num){
-    
     if( document.querySelector('.display').value.length < 4){
-
-
         let timer = document.querySelector('.display').value
         document.querySelector('.display').value = timer + num
-
-
+    }else{
+        document.querySelector('.display').value = ''
     }
-
-
 }
+
+
+// limpa os numeros no timer do microondas e faz o gif sumir caso esteja aparecendo
+function clean(){
+    document.querySelector('.display').value = '';
+
+    if(food.style.display ==='flex'){
+        food.style.display = 'none';
+    }
+}
+
+
+
+
+
+
 
 
